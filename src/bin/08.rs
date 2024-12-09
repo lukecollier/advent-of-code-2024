6f9e05bc76aea7eac44aaede0efd68e69a7e79fa
Luke Collier <leccollier@gmail.com>
2024-12-09 18:07:47 +0000

    feat: Completed day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -39,8 +39,41 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(points.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut world = XYWorld::from_str(input);
+    let mut points: HashSet<(usize, usize)> = HashSet::new();
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for x in 0..world.width {
+        for y in 0..world.height {
+            let ch = world.get(x, y).unwrap();
+            if *ch != '.' {
+                let coords = antennas.entry(*ch).or_insert(vec![]);
+                coords.push((x, y));
+            }
+        }
+    }
+    for coords in antennas.values() {
+        for ((fx, fy), (sx, sy)) in coords.iter().tuple_combinations::<(_, _)>() {
+            let (mut fx_isize, mut fy_isize) = (*fx as isize, *fy as isize);
+            let (mut sx_isize, mut sy_isize) = (*sx as isize, *sy as isize);
+            let fdx = fx_isize - sx_isize;
+            let fdy = fy_isize - sy_isize;
+            let sdx = sx_isize - fx_isize;
+            let sdy = sy_isize - fy_isize;
+            while world.contains(fx_isize, fy_isize) {
+                points.insert((fx_isize as usize, fy_isize as usize));
+                fx_isize += fdx;
+                fy_isize += fdy;
+            }
+            while world.contains(sx_isize, sy_isize) {
+                points.insert((sx_isize as usize, sy_isize as usize));
+                sx_isize += sdx;
+                sy_isize += sdy;
+            }
+        }
+    }
+    world.draw_points(points.iter().copied().collect_vec(), '#');
+    Some(points.len())
 }
 
 #[cfg(test)]
@@ -56,6 +89,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
